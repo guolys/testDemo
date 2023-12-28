ba8eb5a572ccfa43a880205c2daf306969f4f9c3
guoye <475230132@qq.com>
2023-12-28 18:13:54 +0800

    1

---
+++ js/textData.js
@@ -0,0 +1,29 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: guoye
+ * @Date: 2023-12-27 15:29:24
+ * @LastEditors: guoye
+ * @LastEditTime: 2023-12-28 18:13:38
+ */
+var app = new Vue({
+   el: '#app',
+   data: {
+      textTip: "提示信息",
+      dataList: [],
+      optionsQs: ''
+   },
+   mounted () {
+
+   },
+   onLoad () {
+
+   },
+   onShow () {
+
+   },
+   methods: {
+   }
+})
+
+
